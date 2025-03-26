6a73623789b3942c996216e1f7892a112a38bbf0
Hein-Carl Frijlink <hc.frijlink@freedom.nl>
2025-03-26 16:19:14 +0100

    look at that, vitest is compatible with mocha

--- tests/boards.test.js
+++ tests/boards.test.js
@@ -26,39 +26,39 @@ describe("Trello API: CRUD /boards", () => {
     it("POST /boards/ should create a board", async () => {
         const response = await createBoard(key, token, boardName, backgroundColour, visibility)
 
-        expect(response.idOrganization).to.eql(organizationId)
-        expect(response.name).to.eql(boardName)
-        expect(response.closed).to.eql(false)
-        expect(response.prefs.background).to.eql(backgroundColour)
-        expect(response.prefs.permissionLevel).to.eql(visibility)
+        expect(response.idOrganization).toBe(organizationId)
+        expect(response.name).toBe(boardName)
+        expect(response.closed).toBeFalsy()
+        expect(response.prefs.background).toBe(backgroundColour)
+        expect(response.prefs.permissionLevel).toBe(visibility)
         boardId = response.id
     })
 
     it("GET /boards/{boardId} should retrieve a board", async () => {
         const response = await getBoard(boardId, key, token)
 
-        expect(response.idOrganization).to.eql(organizationId)
-        expect(response.name).to.eql(boardName)
-        expect(response.closed).to.eql(false)
-        expect(response.prefs.background).to.eql(backgroundColour)
-        expect(response.prefs.permissionLevel).to.eql(visibility)
+        expect(response.idOrganization).toBe(organizationId)
+        expect(response.name).toBe(boardName)
+        expect(response.closed).toBeFalsy()
+        expect(response.prefs.background).toBe(backgroundColour)
+        expect(response.prefs.permissionLevel).toBe(visibility)
     })
 
     it("PUT /boards/{boardId} should update a board", async () => {
         const response = await updateBoard(boardId, key, token, updatedBoardName, updatedBackgroundColour, updatedVisibility)
 
-        expect(response.idOrganization).to.eql(organizationId)
-        expect(response.name).to.eql(updatedBoardName)
-        expect(response.closed).to.eql(false)
-        expect(response.prefs.background).to.eql(updatedBackgroundColour)
-        expect(response.prefs.permissionLevel).to.eql(updatedVisibility)
+        expect(response.idOrganization).toBe(organizationId)
+        expect(response.name).toBe(updatedBoardName)
+        expect(response.closed).toBeFalsy()
+        expect(response.prefs.background).toBe(updatedBackgroundColour)
+        expect(response.prefs.permissionLevel).toBe(updatedVisibility)
     })
 
     it("PUT /boards/{boardId} should close a board", async () => {
         await closeBoard(boardId, key, token)
 
         const response = await getBoard(boardId, key, token)
-        expect(response.closed).to.be.true
+        expect(response.closed).toBeTruthy()
     })
 
     it("DELETE /boards/{boardId} should delete a board", async () => {
